651dfb976ecd90a3a0cea5eb470c690f4b473126
takakha <66158185+takakha@users.noreply.github.com>
2020-09-18 13:30:10 +0900

    目的ページに遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'situations#index'
   resources :users
-  resources :situations 
+  resources :situations do
+    resources :purposes
   end
+end  
